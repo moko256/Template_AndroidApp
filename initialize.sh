1b30a5fe89519822153d207f4096cfb6619f429b
moko256 <koutaro.mo@gmail.com>
2019-09-21 22:44:07 +0900

    Create java file folder

--- initialize.sh
+++ initialize.sh
@@ -6,6 +6,10 @@ read name
 echo "Replacing placeholder name with \"$name\""
 sed -i s/emptyApp/$name/ app/build.gradle app/src/main/AndroidManifest.xml app/src/main/res/values/strings.xml
 
+javaFileFolder="app/src/main/java/com/github/moko256/$name"
+echo "Creating java folder ($javaFileFolder)"
+mkdir -p $javaFileFolder
+
 echo "Removing .git, README.md and initialize.sh"
 rm -rf .git
 rm README.md
